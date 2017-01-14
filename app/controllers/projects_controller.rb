d02256e26163fb091d8065dd756cac4c2916a364
Luis Briceño <luisabc132@gmail.com>
2017-01-14 12:13:27 +0100

    add show action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,10 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.order(created_at: :desc).limit(10)
   end
+  def show
+    @my_proyect = Project.find_by(id: params[:id])
+    unless @my_proyect
+      render plain: "este no esta es se fue "
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   post '/calculate', to: 'site#calculate'
   get '/calculate', to: 'site#calculate'
   get '/projects', to: 'projects#index'
+  get '/projects/:id', to: 'projects#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
