80736b9484cffa37c54399976980ad057f9d8f27
Luis Briceño <luisabc132@gmail.com>
2017-01-14 12:48:30 +0100

    action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,13 @@ class ProjectsController < ApplicationController
       render plain: "este no esta es se fue "
     end
   end
+  def new
+    @my_project = Project.new
+  end
+
+  def create
+    @my_project = Project.new(:name => params[:project][:name], :description=>params[:project][:description])
+    @my_project.save
+    redirect_to "/projects/#{@my_project.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   post '/calculate', to: 'site#calculate'
   get '/calculate', to: 'site#calculate'
   get '/projects', to: 'projects#index'
+  get '/projects/new', to: 'projects#new'
+  post '/projects', to: 'projects#create'
   get '/projects/:id', to: 'projects#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
