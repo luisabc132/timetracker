524a08ed1d31e1940bbc762c5dae182944d3074f
Luis Briceño <luisabc132@gmail.com>
2017-01-14 10:50:25 +0100

    primeras consultas

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,4 +5,16 @@ class SiteController < ApplicationController
   def contact
     render 'contact'
   end
+  def say_name
+    @name = params[:name]
+  end
+
+  def calculator
+
+  end
+  def calculate
+    @num1 = params[:first_num].to_f
+    @num2 = params[:second_num].to_f
+    @result = @num1 + @num2
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get '/', to: 'site#home'
   get '/contact', to: 'site#contact'
+  get 'say_name/:name', to: 'site#say_name'
+  get '/calculator', to: 'site#calculator'
+  post '/calculate', to: 'site#calculate'
+  get '/calculate', to: 'site#calculate'
+  get '/projects', to: 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
